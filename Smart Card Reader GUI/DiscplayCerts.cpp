dc121d65926febae45635d50945e214cd9455f51
Chad Krauthamer <chadkraut@users.noreply.github.com>
2019-01-08 14:47:54 -0800

    Update DiscplayCerts.cpp

--- Smart Card Reader GUI/DiscplayCerts.cpp
+++ Smart Card Reader GUI/DiscplayCerts.cpp
@@ -1,14 +1,3 @@
-/*
-* An example use of CertSelectCertificate to display certificates on a smart card
-*								using its CSP
-*
-* Copyright (c) 2008 Mounir IDRASSI <mounir.idrassi@idrix.fr>. All rights reserved.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-* or FITNESS FOR A PARTICULAR PURPOSE.
-*
-*/
 
 //#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
 //#define _WIN32_WINNT 0x0500	// Change this to the appropriate value to target other versions of Windows.
